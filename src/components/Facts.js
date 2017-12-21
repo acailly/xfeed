26474cae9872bd1033cc9c66926c132762a7ceac
acailly <ylliac14@gmail.com>
2017-12-21 20:05:40 +0100

    :sparkles: Add React router

---
+++ src/components/Facts.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Facts = () => {
+  return "FACTS"
+}
+
+export default Facts
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./index.css"
 import registerServiceWorker from "./registerServiceWorker"
 import "./rxjs-operators"
 import App from "./components/App"
+import Facts from "./components/Facts"
 import onStartup from "./onStartup"
 
 onStartup().then(() => {
-  ReactDOM.render(<App />, document.getElementById("root"))
+  ReactDOM.render(
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/facts" component={Facts} />
+      </div>
+    </Router>,
+    document.getElementById("root")
+  )
   registerServiceWorker()
 })
