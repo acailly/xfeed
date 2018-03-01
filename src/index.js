e0a89aba1c56c010ac827ca24871adebcf5a1da2
acailly <ylliac14@gmail.com>
2018-03-01 21:10:10 +0100

    :sparkles: Added paie of february 2018

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ addInitialFacts().then(() => {
         <Redirect
           exact
           from="/"
-          to="/le projet assistante maternelle/la paie de janvier 2018"
+          to="/le projet assistante maternelle/la paie de fevrier 2018"
         />
         <Route path="/debug/facts" component={Facts} />
         <Route path="/debug/graph" component={FactsGraph} />
