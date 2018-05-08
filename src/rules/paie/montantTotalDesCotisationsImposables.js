452dced5a8ead8a134a31df4310c7849242a3b30
acailly <ylliac14@gmail.com>
2018-05-08 15:53:57 +0200

    :sparkles: Added an example of what would be a simpler rule declaration syntax

--- src/rules/paie/montantTotalDesCotisationsImposables.js
+++ src/rules/paie/montantTotalDesCotisationsImposables.js
@@ -29,3 +29,52 @@ store
   })
   .catch(err => console.error(err))
   .subscribe()
+
+/*
+TODO ACY ESSAI de format plus simple pour les regles
+rule("montant total des cotisations imposables")
+  .example("
+    la paie d'octobre, est, une paie.
+    la paie de novembre, est, une paie.
+    une paie, est imposable sur, la CSG.
+    une paie, est imposable sur, la RDS.
+    la paie d'octobre, la CSG, 12.
+    la paie d'octobre, la RDS, 6.
+    la paie de novembre, la CSG, 13.
+    la paie de novembre, la RDS, 4.
+  ")
+  .expected("
+    la paie d'octobre, est, une paie.
+    la paie de novembre, est, une paie.
+    une paie, est imposable sur, la CSG.
+    une paie, est imposable sur, la RDS.
+    la paie d'octobre, la CSG, 12.
+    la paie d'octobre, la RDS, 6.
+    la paie de novembre, la CSG, 13.
+    la paie de novembre, la RDS, 4.
+    la paie d'octobre, a pour montant total des cotisations imposables, 18.
+    la paie d'octobre, a pour montant total des cotisations imposables (formatté), 18.00.
+    la paie de novembre, a pour montant total des cotisations imposables, 17.
+    la paie de novembre, a pour montant total des cotisations imposables (formatté), 17.00.
+  ")
+  .when("facts have changed")
+  .find("
+    ?laPaie, est, une paie.
+    une paie, est imposable sur, ?laCotisation.
+    ?laPaie, ?laCotisation, ?montantDeLaCotisation.
+  ")
+  .groupBy("laPaie")
+  .aggregate("
+    laPaie: first(laPaie),
+    montantTotalDesCotisationsImposables: sum(montantDeLaCotisation)
+  ")
+  .transform("
+    laPaie: laPaie,
+    montantTotalDesCotisationsImposables: montantTotalDesCotisationsImposables,
+    montantTotalDesCotisationsImposablesFormatted: toFixed(montantTotalDesCotisationsImposables, 2)
+  ")
+  .replaceFacts("
+    ?laPaie, a pour montant total des cotisations imposables, ?montantTotalDesCotisationsImposables.
+    ?laPaie, a pour montant total des cotisations imposables (formatté), ?montantTotalDesCotisationsImposablesFormatted.
+  ")
+*/
--- src/rules/paie/salaireNetAPayer.js
+++ src/rules/paie/salaireNetAPayer.js
@@ -23,3 +23,47 @@ store
   })
   .catch(err => console.error(err))
   .subscribe()
+
+/*
+TODO ACY ESSAI de format plus simple pour les regles
+rule("salaire net à payer")
+  .example("
+    la paie d'octobre, est, une paie.
+    la paie de novembre, est, une paie.
+    la paie d'octobre, a pour salaire net, 234.
+    la paie de novembre, a pour salaire net, 312.
+    la paie d'octobre, a pour montant total d'indemnité d'entretien, 15.
+    la paie de novembre, a pour montant total d'indemnité d'entretien, 12.
+  ")
+  .expected("
+    la paie d'octobre, est, une paie.
+    la paie de novembre, est, une paie.
+    la paie d'octobre, a pour salaire net, 234.
+    la paie de novembre, a pour salaire net, 312.
+    la paie d'octobre, a pour montant total d'indemnité d'entretien, 15.
+    la paie de novembre, a pour montant total d'indemnité d'entretien, 12.
+    la paie d'octobre, a pour salaire net à payer, 249.
+    la paie d'octobre, a pour salaire net à payer (formatté), 249.00.
+    la paie de novembre, a pour salaire net à payer, 324.
+    la paie de novembre, a pour salaire net à payer (formatté), 324.00.
+  ")
+  .when("facts have changed")
+  .find("
+    ?laPaie, est, une paie.
+    ?laPaie, a pour salaire net, ?salaireNet.
+    ?laPaie, a pour montant total d'indemnité d'entretien, ?montantTotalIndemniteEntretien.
+  ")
+  .transform("
+    laPaie: laPaie,
+    salaireNetAPayer: sum(salaireNet, montantTotalIndemniteEntretien)
+  ")
+  .transform("
+    laPaie: laPaie,
+    salaireNetAPayer: salaireNetAPayer,
+    salaireNetAPayerFormatted: toFixed(salaireNetAPayer, 2)
+  ")
+  .replaceFacts("
+    ?laPaie, a pour salaire net à payer, ?salaireNetAPayer.
+    ?laPaie, a pour salaire net à payer (formatté), ?salaireNetAPayerFormatted.
+  ")
+*/
